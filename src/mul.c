09e147d08ed41c06820134270e92670ee2a2243b
DanceRain <1477364283@qq.com>
2025-03-27 13:23:29 +0800

    稀疏因子为0.2时相比原始代码可提升150倍左右，无稀疏情况提升10倍

--- src/mul.c
+++ src/mul.c
@@ -1,15 +1,49 @@
 #include "mul.h"
+#include <string.h>
+#define BLOCK_SIZE 64
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
+void csrMultiply(int msize, int tidx, int numt, Vector *vec, SparseMatrixCSR *A, SparseMatrixCSR *B, TYPE C[][NUM], TYPE temp[][NUM]){
+	for (int block_i = tidx * BLOCK_SIZE; block_i < msize; block_i += numt * BLOCK_SIZE) {
+        int block_end = (block_i + BLOCK_SIZE < msize) ? block_i + BLOCK_SIZE : msize;
+        for (int i = block_i; i < block_end; i++) {
+            TYPE local_temp[NUM] = {0};
+            for (int j = A->row_ptr[i]; j < A->row_ptr[i + 1]; j++) {
+                int a_col = A->col_idx[j];
+                TYPE a_val = A->values[j];
+                for (int k = B->row_ptr[a_col]; k < B->row_ptr[a_col + 1]; k++) {
+                    int b_col = B->col_idx[k];
+                    TYPE b_val = B->values[k];
+                    local_temp[b_col] += a_val * b_val;
+                }
+            }
+            for (int col = 0; col < NUM; col++) {
+                C[i][col] += local_temp[col];
+            }
+        }
+    }
+}
 
 void mul(int msize, int tidx, int numt, Vector *vec, TYPE a[][NUM], TYPE b[][NUM], TYPE c[][NUM], TYPE t[][NUM])
 {
-    int i,j,k;
+	for (int bi = tidx * BLOCK_SIZE; bi < msize; bi += numt * BLOCK_SIZE) {
+        for (int bk = 0; bk < msize; bk += BLOCK_SIZE) {
+            for (int bj = 0; bj < msize; bj += BLOCK_SIZE) {
+                int i_end = min(bi + BLOCK_SIZE, msize);
+                int k_end = min(bk + BLOCK_SIZE, msize);
+                int j_end = min(bj + BLOCK_SIZE, msize);
 
-    for (i = tidx; i < msize; i = i + numt) {
-        for (j = 0; j < msize; j++) {
-            for (k = 0; k < msize; k++) {
-                c[i][j] = c[i][j] + a[i][k] * b[k][j];
+                for (int i = bi; i < i_end; i++) {
+                    for (int k = bk; k < k_end; k++) {
+                        TYPE a_ik = a[i][k];
+                        #pragma omp simd
+                        for (int j = bj; j < j_end; j++) {
+                            c[i][j] += a_ik * b[k][j];
+                        }
+                    }
+                }
             }
-            vector_append(vec, c[i][j]);
         }
     }
 }
--- src/thrmodel.c
+++ src/thrmodel.c
@@ -8,6 +8,8 @@
 
 #define xstr(s) x_str(s)
 #define x_str(s) #s
+#define SPARSE_THRESHOLD 0.1
+
 
 typedef struct tparam
 {
@@ -16,7 +18,61 @@ typedef struct tparam
 	int tidx;
 	int numt;
 	Vector *vec;
+	SparseMatrixCSR *sparseA;
+        SparseMatrixCSR	*sparseB;
+	_Bool isSparse;
 } _tparam;
+
+int isSparse(TYPE mat[][NUM], int msize) {
+    int nonZero = 0, total = msize * msize;
+    for (int i = 0; i < msize; i++) {
+        for (int j = 0; j < msize; j++) {
+            if (mat[i][j] != 0) nonZero++;
+            if ((double)nonZero / total > SPARSE_THRESHOLD) return 0; 
+        }
+    }
+    return 1; 
+}
+
+void convertToCSR(TYPE mat[][NUM], int msize, SparseMatrixCSR *csrMat) {
+    int nnz = 0;
+
+    for (int i = 0; i < msize; i++) {
+        for (int j = 0; j < msize; j++) {
+            if (mat[i][j] != 0) {
+                nnz++;
+            }
+        }
+    }
+    csrMat->num_nonzeros = nnz;
+    csrMat->values = (TYPE *)malloc(nnz * sizeof(TYPE));
+    csrMat->col_idx = (int *)malloc(nnz * sizeof(int));
+    csrMat->row_ptr = (int *)malloc((msize + 1) * sizeof(int));
+
+    int pos = 0;
+    csrMat->row_ptr[0] = 0; 
+
+    for (int i = 0; i < msize; i++) {
+        for (int j = 0; j < msize; j++) {
+            if (mat[i][j] != 0) {
+                csrMat->values[pos] = mat[i][j];
+                csrMat->col_idx[pos] = j;  
+                pos++;
+            }
+        }
+        csrMat->row_ptr[i + 1] = pos; 
+    }
+}
+
+void freeCSR(SparseMatrixCSR *csrMat) {
+    free(csrMat->values);
+    free(csrMat->col_idx);
+    free(csrMat->row_ptr);
+
+    csrMat->values = NULL;
+    csrMat->col_idx = NULL;
+    csrMat->row_ptr = NULL;
+}
 
 void *ThreadFunction(void *ptr)
 {
@@ -27,17 +83,18 @@ void *ThreadFunction(void *ptr)
 	assert(par->c != NULL);
 	assert(par->t != NULL);
 	assert( (par->msize % par->numt) == 0);
-
-	mul(par->msize, par->tidx, par->numt, par->vec, par->a, par->b, par->c, par->t);
-
-	// destroy vector here
-	// vector_destroy(par->vec);
-
+	if (par->isSparse) {
+		//printf("sparse mutiply\n");
+		csrMultiply(par->msize, par->tidx, par->numt, par->vec, par->sparseA, par->sparseB, par->c, par->t);
+	}else{
+	    //printf("dense mutiply\n");
+	    mul(par->msize, par->tidx, par->numt, par->vec, par->a, par->b, par->c, par->t);
+	}
 	pthread_exit((void *)0);
 }
 
 
-void ParallelMultiply(int msize, TYPE a[][NUM], TYPE b[][NUM], TYPE c[][NUM], TYPE t[][NUM])
+void ParallelMultiply(int msize, TYPE a[][NUM], TYPE b[][NUM], TYPE c[][NUM], TYPE t[][NUM], _Bool isSparse)
 {
 	int NTHREADS = MAXTHREADS;
 	int MSIZE = NUM;
@@ -51,21 +108,29 @@ void ParallelMultiply(int msize, TYPE a[][NUM], TYPE b[][NUM], TYPE c[][NUM], TY
 
 	GetModelParams(&NTHREADS, &MSIZE, 0);
 
+	SparseMatrixCSR A, B;
+        convertToCSR(a, MSIZE, &A);
+        convertToCSR(b, MSIZE, &B);
 	for (tidx = 0; tidx < NTHREADS; tidx++) {
 		par[tidx].msize = MSIZE;
 		par[tidx].numt = NTHREADS;
 		par[tidx].tidx = tidx;
 		par[tidx].vec = vector_create();
+		par[tidx].sparseA = &A;
+		par[tidx].sparseB = &B;
 		par[tidx].a = a;
 		par[tidx].b = b;
 		par[tidx].c = c;
 		par[tidx].t = t;
+		par[tidx].isSparse = isSparse;
 		tret[tidx] = pthread_create(&ht[tidx], NULL, (void*)ThreadFunction, (void*) &par[tidx]);
 	}
 
 	for (tidx = 0; tidx < NTHREADS; tidx++) {
 		rc = pthread_join(ht[tidx], (void **)&status);
 	}
+	freeCSR(&A);
+	freeCSR(&B);
 }
 
 extern int getCPUCount();
