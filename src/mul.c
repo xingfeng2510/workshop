216e9e8d2e4895c85287d55f15a34ce932f3a3e0
bernard jiang <bernardjiang5@outlook.com>
2025-04-25 01:05:12 +0800

    perf: resche loop

--- src/mul.c
+++ src/mul.c
@@ -3,12 +3,19 @@
 void mul(int msize, int tidx, int numt, Vector *vec, TYPE a[][NUM], TYPE b[][NUM], TYPE c[][NUM], TYPE t[][NUM])
 {
     int i,j,k;
-
-    for (i = tidx; i < msize; i = i + numt) {
-        for (j = 0; j < msize; j++) {
-            for (k = 0; k < msize; k++) {
-                c[i][j] = c[i][j] + a[i][k] * b[k][j];
+    
+    for (k = 0; k < msize; k++) {
+        for (i = tidx; i < msize; i += numt) {
+            TYPE a_val = a[i][k];
+            for (j = 0; j < msize; j++) {
+                c[i][j] += a_val * b[k][j];
             }
+        }
+    }
+    
+    // 后处理vector_append
+    for (i = tidx; i < msize; i += numt) {
+        for (j = 0; j < msize; j++) {
             vector_append(vec, c[i][j]);
         }
     }
